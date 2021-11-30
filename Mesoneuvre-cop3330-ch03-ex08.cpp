dca662b8ae0cf48d476e471b7f07a2e77a865184
Dynie Mesoneuvre <50300000+DynieM@users.noreply.github.com>
2021-11-29 23:32:24 -0500

    Create Mesoneuvre-cop3330-ch03-ex08.cpp

---
+++ Mesoneuvre-cop3330-ch03-ex08.cpp
@@ -0,0 +1,35 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 5 Solution
+ *  Copyright 2021 Dynie Mesoneuvre
+ */
+
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    //initializing int variable
+    int num;
+    //asking for the input and setting it as int num
+    cout << "Input Number: ";
+    cin >> num;
+
+    //if statement based on whether or not num is even
+    if( num % 2 == 0)
+    {
+        //output statement
+        cout << num << " is an even number.";
+    }
+
+    //else statement if num is not even
+    else
+    {
+        //output statement
+        cout << num << " is an odd number.";
+    }
+
+
+    return 0;
+}
